ad349c7f58b861497fd502cb90d040d8ed92a8f6
Isaac Orme <isorme1@gmail.com>
2017-12-14 17:28:49 -0800

    migration-fix

--- db/schema.rb
+++ db/schema.rb
@@ -51,5 +51,6 @@ ActiveRecord::Schema.define(version: 20171210072314) do
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", [nil], name: "index_users_on_unlock_token", unique: true
 
 end
